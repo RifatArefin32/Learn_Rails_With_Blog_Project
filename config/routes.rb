988f7d36772aa3f23dacea802696ad11c95678dc
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-10-11 15:59:24 +0600

    Deletion completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get "/blog_posts", to: "blog_posts#index", as: :blog_posts
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  patch "/blog_posts/:id", to: "blog_posts#update"
+  delete "/blog_posts/:id", to: "blog_posts#destroy"
+  get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   post "/blog_posts", to: "blog_posts#create"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
